dbe142382fc11904e2e688c7b18a26e01089bac6
quangnx <quangnx@ichiba.vn>
2023-01-28 14:54:17 +0700

    update

--- jenkins/command.sh
+++ jenkins/command.sh
@@ -1,3 +1,4 @@
 docker run -d —name jenkins -p 8080:8080 -p 50000:50000 -v /Users/adqua/Desktop/jenkins:/var/jenkins_home jenkins/jenkins:lts
 
+#fix 403 error
 sudo chown -R 1000 /Users/adqua/Desktop/jenkins
